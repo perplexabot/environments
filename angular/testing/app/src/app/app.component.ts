06dacdb663d5e3ebe0db2bdd8922502e8310b5e9
Basil Masri <basil.masri@sony.com>
2024-03-22 12:05:11 -0700

    playground angular env

--- angular/testing/app/src/app/app.component.ts
+++ angular/testing/app/src/app/app.component.ts
@@ -1,13 +1,22 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { FormControl, ReactiveFormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  styleUrl: './app.component.css',
+  imports: [ReactiveFormsModule, RouterOutlet]
 })
 export class AppComponent {
   title = 'app';
+  oh_no = 'syck!';
+  attempt: string | null = '';
+
+  f = new FormControl('');
+
+  dothing() {
+      this.attempt = this.f.value
+  }
 }
