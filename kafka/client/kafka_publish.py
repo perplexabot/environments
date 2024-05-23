cff80d397c5211aefbbe31ab9982fb41fd3a67a9
Basil Masri <basil.masri@sony.com>
2024-05-23 15:26:20 -0700

    adding more kafka stuff

---
+++ kafka/client/kafka_publish.py
@@ -0,0 +1,26 @@
+"""
+Publish event to kafka. Test script.
+"""
+
+from confluent_kafka import Producer
+
+topic = "quickstart-events"
+server = '127.0.0.1:9092'
+
+p = Producer({'bootstrap.servers': server})
+
+
+def delivery_report(err, msg):
+    if err is not None:
+        print(f'Message delivery failed with err: {err}')
+    else:
+        print(f'Message delivery was successful to {msg.topic()} [{msg.partition()}]')
+
+
+datas = ['hello', 'bye']
+
+for data in datas:
+    p.poll(0)
+    p.produce(topic, data.encode('utf-8'), callback=delivery_report)
+
+p.flush()
